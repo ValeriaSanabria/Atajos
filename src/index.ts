53402c49a6baedaa4ff8296f50b6c738221692a2
Valeria Sanabria <103613127+ValeriaSanabria@users.noreply.github.com>
2022-06-06 21:33:40 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+function mayorValor(entrada: number []) {
+  let total: number = entrada.length;
+  let mayor: number = 0;
+  if (total > 0) {
+    mayor = entrada[0];
+    if (total > 1) {
+      for( let i = 0; i < total; i++) {
+        if(entrada[i] > mayor)
+        mayor = entrada[i];
+      }
+    }
+  }
+  console.log("mayor valor: " + mayor)
+}
+
+mayorValor([5,8,9,6,4,5,8,3]);
